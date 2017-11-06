2ec31cd7d15e0245671528c72ee804af187cf66e
takagotch <dyaccb@gmail.com>
2017-11-07 03:22:15 +0900

    open-uri

---
+++ list/FileUnite.cp.rb
@@ -0,0 +1,25 @@
+require "tempfile"
+require "fileutils"
+
+temp = Tempfile.new("working")
+File.foreach(ARGV[0]) do |line|
+  temp << line unless line =~ /^\s*#/
+end
+
+temp.close
+FileUtils.cp(ARGV[0],"#{ARGV[0].bak}")
+FileUtils.mv(temp.path,ARGV[0])
+
+
+
+
+#require "tempfile"
+#require "fileutils"
+#temp = Tempfile.new("working")
+#File.foreach(ARGV[0]) do |line|
+#  temp << line unless line =~/^\s*#/
+#end
+#
+#temp.close
+#FileUtils.mv(temp.path,ARGV[0])
+#
