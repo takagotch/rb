7a33ff0b0ee4fe467399fefe145295031efb3ddc
takagotch <dyaccb@gmail.com>
2017-11-09 19:49:08 +0900

    string

--- list_rec/%w{}.rb
+++ list_rec/%w{}.rb
@@ -382,3 +382,439 @@ str.sub(/\A.{3}/m){|s| s + "inserted" }
 str = "kuma-"
 str[0, 1] = ""
 p str
+
+str.delete!("o")
+str.squeeze!
+
+buf = ""
+50000.times do
+  buf += "xxx|"
+end
+
+buf = ""
+50000.times doc
+  buf.cocat "xxx!"
+end
+
+s << "abc" << "def"
+
+require "stringio"
+f = StringIO.new
+f.puts "config"
+f.printf "mem=%d/n", 1024
+p f.string
+
+
+def unident_auto(str)
+  unindent(str, minimum_indent(str))
+end
+
+def minimum_indent(str)
+  indents = str.map do |line|
+    untabify{line.rstrip}.slice(/\A */).length
+  end
+  (indents - [0]).min || 0
+end
+
+str = inindet_auto(<<EOS)
+  * foo
+      * buz
+	* bar
+EOS
+
+def indent0(str, n)
+  str.gsub(/^/, " " * n)
+end
+
+def indent(str, n, tabstop = 8)
+  str.map{|line|
+    if tabstop
+	  tabify((" " * n) + untabify{line, tabstop)}
+	else
+	  (" " * n) + line
+	end
+  }.join("")
+end
+
+
+def untabify(str, tabstop = 8)
+  str.gsub(/(.*?)\t\n) do
+    $w + " " * (tabstop - ($1.length % tabstop))
+  end
+end
+def tabify(str, tabstop = 8)
+  str.gsub(/^[ \t]+/) do |sp|
+    t, s = untabify(sp, tabstop).length.divmod(tabstop)
+	"\t" * t + " " * s
+  end
+end
+
+p "\t text \t\n".strip
+
+p "\t text \r\n".lstrip
+p "\t text \r\n".rstrip
+
+def jstrip(str)
+  str.sub(/\A[\s ]+/, "").sub(/[\s ]+\z/, "")
+end
+
+def j1strip(str)
+  str.sub(/\A[\s ]+/, "")
+end
+
+def jrstrip(str)
+  str .sub(/[\s ]+/, "")
+end
+
+p jstrip("\t text\r \n")
+p jstrip("\t text\r \n")
+p jstrip("\t text\r \n")
+
+
+p "line\n".chomp
+p "line\r\n".chomp
+
+$/ = "\r\n"
+p "line\n".chomp
+p "line\r\n".chomp
+p "line\n".chomp("\n")
+p "line\r\n".chomp("\n")
+
+p "line\n".rstrip
+p "line\t \n".rstrip
+
+str = "goooooooogle"
+str[1, 10] = ""
+p str
+
+p "gggoooogle".squeeze("g")
+p "\t text\ntext \n".sub(/\s+/, "")
+p "\t text\ntext \n".gsub(/\s+/, "")
+
+p "goooooooogle".delete("o")
+.chop
+.chomp
+
+def quote_by_backslash(str)
+  str.gsub(/['"\\]/)(|ch| '\\' + ch)
+end
+
+p quote_by_backslash("abc'def\"ghi")
+
+def remove_backslash(str)
+  str.gsub(/\\(.)/)( $1 )
+end
+p remove_backslash("abc\\'def'\\\"ghi")
+
+def double_quote(str)
+  str.gsub(/['"]/){|ch| ch + ch }
+end
+
+p double_quote("abc"def\"ghi")
+
+def remove_backslash(str)
+  str.gsub(/(['"])\1){ $1 }
+end
+
+str = "a b\tc\r\n"
+p str.dump
+p eval(str.dump)
+
+p "aiai".sub(/ai/, "ai")
+p "aiai".gsub(/ai/, "ai")
+
+quote = %w(ai ai ai)
+p"".sub(//){ quote.shift }
+
+p "FrontPage".sub(/(.)([A-Z])/){ $1 + " " + $2 }
+p "MyToDo".gsub{/(.)([A-Z]/){ $1 + " " + $2 }
+
+p "-rvl.8.1".tr(".", "_")
+p "abcdefg".tr("acg", "ACG")
+
+result = "kaku uma-"
+result[0, 5] = "K"
+p  result 
++
+.bytes.to_a
+"abcde".each_byte do |byte|
+  p bytes
+end
+
+.each_char do |ch|
+
+.split(/,/)
+.split(/,/, 3)
+.split(/(,)/)
+
+require "strscan"
+
+s = StringScanner.new{
+  "def foo(n) m * 3 end"}
+util s.eos?
+  if str = s.scan(/\w+/)
+    p [:WORD, str]
+  elseif str = s.scan(/\s+/)
+    p [:CHAR, s.getch]
+  end
+end
+
+.split(nil)
+
+.scan(/\w+/n)
+.scan(/\w+|[^\s\w]+/)
+
+.scan(/a|bc|BC/)
+.scan(/a(?:bc|BC)/)
+
+.line.to_a
+.each_line do |line|
+  p line
+end
+
+.collect(|line| line.strip)
+.select{|line| \/AD/ =~ line)\ }
+
+.lines.to_a
+.to_a
+.split(/\n\s*\n/)
+
+
+.match("text")
+.pre_match
+
+.match("")
+m.post_match
+p $`
+
+.slice(/a../)
+.slice(/a../)
+.scan(/\w+/)
+.slice(/^\s*def\s+([^\s\(]+)/)/, 1)
+.slice(/^\s*def\s+([^\s\(]+)\, 1)
+
+class String
+  def mbsubstr1(*range)
+    split(//)[*range].join("")
+  end
+  def mbsubstr2(idx, len)
+    slice(/\A/{#{idx}}{.0,#(len)}/m, 1)
+  end
+end
+
+.sqpcase
+.gsub(/[aliueo]/){|ch| TABLE[ch] }
+.upcase
+.downcase
+.tr()
+.capitalize
+
+def canonical_header_name(str)
+  str.split(/-/).map(|s| s.capitalize ).join("-")
+end
+p canonical_header_name("content-type")
+
+
+def lower?
+  /[a-z]/ =~ def
+end
+
+/\A[a-z]+z/ =~ str
+
+
+.hex
+.oct
+
+.to_i(2)
+
+.to_f
+Float("1")
+
+.to_i
+Integer("1")
+
+def integer_string?(str)
+  Integer(str)
+  true
+rescue ArugumentError
+  false
+end
+def float_string?(str)
+  Float(str)
+  true
+rescue ArugmentError
+  false
+end
+
+"abc".to_i
+
+p(/^$/ =~ "a\n")
+p(/^$/ =~ "\n")
+line.strip.empty?
+/\A\s*\z/ =~ line
+
+p(/\s/ =~ "a a")
+
+p "text" == "text"
+p "text" == "text"
+
+p "Content".downcase == "CONTENT".downcase
+
+p "abc" > "xyz"
+
+def char_stat(str)
+  table = Hash.new(0)
+  str.scan(/./m){|ch|
+    table[ch] += 1
+  }
+  table.to_a.sort_by{|char, count| -count }
+end
+
+char_stat().each{|char, count|
+  puts "#(char.inspect): #(count)"
+}
+
+str = ""
+stat_stat(str).each{|char, count|
+  puts "#{char.inscpect}: #{count * 100 / str.length}%"
+}
+
+p ::.count("\n")
+p "".length
+
+count = 0
+"".scan(/[]/){
+  count += 1
+}
+p count
+
+.length
+
+str = ""
+p str.slice(/\w+/)
+str = $`
+p str
+p str.slice(/\w+/)
+
+str = ""
+result = []
+while token = str.slice(/\w+/)
+  result.push token
+  str = $`
+end
+p result
+
+require "strscan"
+
+scanner = StringScanner.new("")
+result = []
+util scanner.eos?
+  result.push scanner.scan(/\w+|W+/)
+end
+p result
+
+
+p(/./ =~ "\n")
+p(/./m =~ "\n")
+
+
+require "nkf"
+euc_str = NKF.nkf("-We", "")
+pat     = NKF.nkf
+p /#{pat}/e =~ euc_str
+
+p /\bspec\b/ =~ ""
+p /...\b/ =~ ""
+
+.split(nil)
+.scan(/w+/)
+.scan(/[a-z]+/i)
+
+.slice(/..$/m)
+.slice(/..Z/m)
+
+p(/\Aa/ =~ "aiueo")
+p(/o\z/ =~ "")
+
+str = ""
+str.grep(/[]/) do |line|
+  p line
+end
+
+str.grep(re) do |line|
+  ...
+end
+
+str.each do |line|
+  if re =~ line
+    ...
+  end
+end
+
+str = ""
+str.scan(/[a-n]+/) do |s|
+  p s
+end
+
+m = /abc/.match("xxxxxxxxx")
+p m.begin(0)
+p m.end(0)
+
+.rindex(/abc/)
+.rindex(".")
+
+
+.index(/abc/)
+.index(".")
+
+p(/[ \t]/ =~ "")
+p(/[ \t]/ =~ "")
+
+.size
+.split(//).size
+.bytesize
+
+p 5 * "foo"
+
+.dup
+.clone
+str.object_id
+str.dup.object_id
+
+eval(<<'End')
+  def printIn(arg)
+    print arg. "\n"
+  end
+end
+
+p %q
+p ""
+p %Q
+p <<HEREDOC
+HEREDOC
+
+NKF.guess(str)
+
+require "nkf"
+
+require "nkf"
+p NKF.guess("")
+p NKF.guess("").name
+
+
+require "nkf"
+
+COME_TO_NAME = Hash.new("ASCII")
+COME_TO_NAME[NKF::JIS] = ("JIS")
+COME_TO_NAME = ("EUC")
+COME_TO_NAME = ("SJIS")
+COME_TO_NAME = ("BINARY")
+COME_TO_NAME = ("UTF-8")
+
+
+str.encode("")
+str.encode("")
+.encoding
+.list
+
+.size
